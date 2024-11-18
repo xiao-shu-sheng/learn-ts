42a5a8002a3fef52d773faabaeec31c5efc6fe8e
wszq314618 <2418356743@qq.com>
2024-11-18 15:26:33 +0800

    修改

--- src/13.ts
+++ src/13.ts
@@ -1,38 +1,39 @@
-// 类型推断|类型别名
+{
+  // 类型推断|类型别名
+  // 鼠标放到变量上就可以看到推断出的类型
+  let numAs = 1
+
+  // 报错，推断出类型为number
+  numAs = '12'
 
 
-// 鼠标放到变量上就可以看到推断出的类型
-let numAs = 1
+  // 没有给变量类型，也没有赋值，默认推断为any类型
+  let numAs1
 
-// 报错，推断出类型为number
-numAs = '12'
-
-
-// 没有给变量类型，也没有赋值，默认推断为any类型
-let numAs1
-
-numAs1 = 12
-numAs1 = '12'
-numAs1 = true
-numAs1 = []
-numAs1 = {}
+  numAs1 = 12
+  numAs1 = '12'
+  numAs1 = true
+  numAs1 = []
+  numAs1 = {}
 
 
-// 类型别名, 可以定义任何类型
-type s = string | number
-let strAs:s = '121'
+  // 类型别名, 可以定义任何类型
+  type s = string | number
+  let strAs:s = '121'
 
 
-// type不能使用extends来继承，interface可以，type只能用交叉类型
-// type可以使用联合类型，interface不行
-// interface遇到重名的会合并，type遇到重名的报错
+  // type不能使用extends来继承，interface可以，type只能用交叉类型
+  // type可以使用联合类型，interface不行
+  // interface遇到重名的会合并，type遇到重名的报错
 
 
-// type高级用法
-// extends在type中是包含的意思，左边的值会作为右边类型的子类型
-type numType = 1 extends number ? 1 : 0
-type numType2 = 1 extends any ? 1 : 0
-type numType3 = 1 extends unknown ? 1 : 0
-type numType4 = 1 extends Object ? 1 : 0
-type numType5 = 1 extends Number ? 1 : 0
-type numType6 = 1 extends never ? 1 : 0
+  // type高级用法
+  // extends在type中是包含的意思，左边的值会作为右边类型的子类型
+  type numType = 1 extends number ? 1 : 0
+  type numType2 = 1 extends any ? 1 : 0
+  type numType3 = 1 extends unknown ? 1 : 0
+  type numType4 = 1 extends Object ? 1 : 0
+  type numType5 = 1 extends Number ? 1 : 0
+  type numType6 = 1 extends never ? 1 : 0
+
+}
--- src/17.ts
+++ src/17.ts
@@ -1,53 +1,66 @@
-function add<T>(a: T, b: T) {
-    //return a + b // 报错
+{
+    function add<T>(a: T, b: T) {
+        //return a + b // 报错原因是因为ts不知道类型参数T是否支持加法操作
+    }
+
+    // 泛型约束
+    function addLimit<T extends number>(a: T, b: T) {
+        return a + b // 报错
+    }
+
+    function addLimit2<T extends string>(a: T, b: T) {
+        return a + b // 报错
+    }
+
+    function addLimit3<T extends number>(a: T, b: T) {
+        return (a as number) + (b as number)
+    }
+
+    function addLimit4<T extends string>(a: T, b: T) {
+        return (a as string) + (b as string)
+    }
+
+
+    // 泛型约束
+    interface LenLimit{
+        length:number
+    }
+    function fnLimit<T extends LenLimit>(a: T) {
+        console.log(a.length)
+    }
+    fnLimit("111")
+    fnLimit([1, 1, 1])
+
+
+
+
+    // keyof约束
+    let person2 = {
+        name: "天明",
+        age: 12
+    }
+
+    type PKey = keyof typeof person2
+   
+
+    function ob<T extends object, K extends keyof T>(obj: T, key: K) {
+        console.log(obj[key])
+    }
+    ob(person2, 'name')
+
+
+
+    // 泛型可选
+    interface Data2 {
+        name: string,
+        age: number,
+        sex: boolean,
+    }
+
+    // for in
+    type Options<T extends object> = {
+        readonly [Key in keyof T]?:T[Key]
+    }
+    type BOptions = Options<Data2>
+
 }
-
-// 泛型约束
-function addLimit<T extends number>(a: T, b: T) {
-    return a + b // 报错
-}
-function addLimit2<T extends string>(a: T, b: T) {
-    return a + b // 报错
-}
-
-
-// 泛型约束
-interface LenLimit{
-    length:number
-}
-function fnLimit<T extends LenLimit>(a: T) {
-    console.log(a.length)
-}
-fnLimit("111")
-fnLimit([1, 1, 1])
-
-
-
-
-// keyof约束
-let person2 = {
-    name: "天明",
-    age: 12
-}
-
-type PKey = keyof typeof person2
-
-function ob<T extends object, K extends keyof T>(obj: T, key: K) {
-    console.log(obj[key])
-}
-ob(person2, 'name')
-
-
-
-// 泛型可选
-interface Data2 {
-    name: string,
-    age: number,
-    sex: boolean,
-}
-
-// for in
-type Options<T extends object> = {
-    readonly [Key in keyof T]?:T[Key]
-}
-type BOptions = Options<Data2>
